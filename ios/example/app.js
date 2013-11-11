4ffea68cdc00d8c5bc951986278c1da13928aea9
srahim <srahim@appcelerator.com>
2013-11-11 21:20:26 +0530

    [TIMOB-14617]Updating test code.

--- ios/example/app.js
+++ ios/example/app.js
@@ -1,9 +1,6 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
+//Sample test app 
 
-var backgroundTransfer = require("com.appcelerator.urlSession");
+var urlsessionModule = require("urlSession");
 var session;
 var keyWindow = Ti.UI.createWindow({backgroundColor:"white"});
 var ind=Titanium.UI.createProgressBar({
@@ -37,38 +34,30 @@ var b1 = Titanium.UI.createButton({
 keyWindow.add(b1);
 b1.addEventListener('click', function()
 {
-	var sessionConfig = backgroundTransfer.createURLSessionBackgroundConfiguration("com.test.test2");
-    session = backgroundTransfer.createURLSession(sessionConfig);
+	var sessionConfig = urlsessionModule.createURLSessionBackgroundConfiguration("com.appcelerator.test");
+    session = urlsessionModule.createURLSession(sessionConfig);
 	
 	if (session != null) {
-		backgroundTransfer.backgroundDownloadTaskWithURL(session,"http://yourshot.nationalgeographic.com/exposure/content/photo/photo/1879122_uploadsmember258527yourshot-258527-1879122jpg_z5n3nv2g7piofy6fkaewns5su56efp7ahougetfp4dbeloye3ueq_2880x1922.jpg");
+		urlsessionModule.backgroundDownloadTaskWithURL(session,"http://yourshot.nationalgeographic.com/exposure/content/photo/photo/1879122_uploadsmember258527yourshot-258527-1879122jpg_z5n3nv2g7piofy6fkaewns5su56efp7ahougetfp4dbeloye3ueq_2880x1922.jpg");
 	}
 });
 
 Ti.App.iOS.addEventListener("downloadprogress", function(e) {
-	Ti.API.info("e.session" + e.session);
-	Ti.API.info("e.taskIdentifier" + e.taskIdentifier);
-	Ti.API.info("e.bytesWritten" + e.bytesWritten);
-	Ti.API.info("e.totalBytesWritten" + e.totalBytesWritten);
-	Ti.API.info("e.totalBytesExpectedToWrite"+ e.totalBytesExpectedToWrite);
+	Ti.API.info("download progres " + JSON.stringify(e));
 	ind.value = (e.totalBytesWritten/e.totalBytesExpectedToWrite);
 });
 
-Ti.App.iOS.addEventListener("downloadfinished", function(e) {
-	Ti.API.info("e.taskIndentifier"+ e.taskIdentifier);
+Ti.App.iOS.addEventListener("downloadcompleted", function(e) {
+	Ti.API.info("download completed " + JSON.stringify(e));
 	Ti.API.info("e.url"+ e.url);
 	alert("task : " + e.taskIdentifier + " completed. Download to url : " + e.data);
 	
 	imageView.image= e.data;
-	backgroundTransfer.invalidateAndCancel(session);
+	urlsessionModule.invalidateAndCancel(session);
 });
 
 Ti.App.iOS.addEventListener('sessioncompleted', function(e) {
-	Ti.API.info("e.taskIndentifier"+ e.taskIdentifier);
-	Ti.API.info("e.success"+ e.success);
-	Ti.API.info("e.errorCode"+ e.errorCode);
-	Ti.API.info("e.message"+ e.message);
-	Ti.API.info("e.success"+ e.success);
+	Ti.API.info("sessioncompleted " + JSON.stringify(e));
 	if (e.sucess) {
 		alert("Background Transfer Completed succesfully.");
 	}
