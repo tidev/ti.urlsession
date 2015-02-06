9e035bdbfa096945cadef7d0cfd23a732186f933
cheekiatng <cng@appcelerator.com>
2015-02-06 16:30:31 +0800

    corrected travis.yml

--- ios/xcode/urlSession/src/js_Foundation/js_NSURLSessionConfiguration_P.m
+++ ios/xcode/urlSession/src/js_Foundation/js_NSURLSessionConfiguration_P.m
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2013-2015 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  *
